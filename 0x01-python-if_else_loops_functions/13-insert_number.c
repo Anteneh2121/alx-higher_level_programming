d7ef42c712920aa83f60eca93b4657f5a790ce49
root <antukassaw1@gmail.com>
2021-11-16 12:49:39 +0000

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,43 +14,43 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-  listint_t *new_head = malloc(sizeof(listint_t));
-  listint_t *tmp = *head;
+listint_t *new_head = malloc(sizeof(listint_t));
+listint_t *tmp = *head;
 
-  if (new_head == NULL)
-    {
-      return (NULL);
-    }
-  if (tmp == NULL)
-    {
-      new_head->n = number;
-      new_head->next = NULL;
-      (*head) = new_head;
-      return (new_head);
-    }
-  if (tmp->next == NULL || number == 0)
-    {
-      if (number < tmp->n)
-	{
-	  new_head->n = number;
-	  new_head->next = tmp;
-	  (*head) = new_head;
-	  return (new_head);
-	}
-    }
-  while (tmp->next)
-    {
-      if ((number >= tmp->n) && (number <= tmp->next->n))
-	{
-	  new_head->n = number;
-	  new_head->next = tmp->next;
-	  tmp->next = new_head;
-	  return (new_head);
-	}
-      tmp = tmp->next;
-    }
-  new_head->n = number;
-  new_head->next = NULL;
-  tmp->next = new_head;
-  return (new_head);
+if (new_head == NULL)
+{
+return (NULL);
+}
+if (tmp == NULL)
+{
+new_head->n = number;
+new_head->next = NULL;
+(*head) = new_head;
+return (new_head);
+}
+if (tmp->next == NULL || number == 0)
+{
+if (number < tmp->n)
+{
+new_head->n = number;
+new_head->next = tmp;
+(*head) = new_head;
+return (new_head);
+}
+}
+while (tmp->next)
+{
+if ((number >= tmp->n) && (number <= tmp->next->n))
+{
+new_head->n = number;
+new_head->next = tmp->next;
+tmp->next = new_head;
+return (new_head);
+}
+tmp = tmp->next;
+}
+new_head->n = number;
+new_head->next = NULL;
+tmp->next = new_head;
+return (new_head);
 }
