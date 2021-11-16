bef675a09729556e955245b455836ca53e660c72
root <antukassaw1@gmail.com>
2021-11-16 13:02:14 +0000

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,56 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- * insert_node - insert node in a linked list
- *
- * @head: pointer to the first node of the linked list
- *
- * @number: integer value of the new node to insert
- *
- * Return: the head of the linked list with the new node inserted
+ * insert_node - inserts a number into a sorted list
+ * @head: pointer to the list
+ * @number: number to add
+ * Return: address of the added node else NULL
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *new_head = malloc(sizeof(listint_t));
-listint_t *tmp = *head;
+  listint_t *old, *new, *current;
 
-if (new_head == NULL)
-{
-return (NULL);
-}
-if (tmp == NULL)
-{
-new_head->n = number;
-new_head->next = NULL;
-(*head) = new_head;
-return (new_head);
-}
-if (tmp->next == NULL || number == 0)
-{
-if (number < tmp->n)
-{
-new_head->n = number;
-new_head->next = tmp;
-(*head) = new_head;
-return (new_head);
-}
-}
-while (tmp->next)
-{
-if ((number >= tmp->n) && (number <= tmp->next->n))
-{
-new_head->n = number;
-new_head->next = tmp->next;
-tmp->next = new_head;
-return (new_head);
-}
-tmp = tmp->next;
-}
-new_head->n = number;
-new_head->next = NULL;
-tmp->next = new_head;
-return (new_head);
+  if (head == NULL)
+    return (NULL);
+
+  new = malloc(sizeof(listint_t));
+  if (new == NULL)
+    return (NULL);
+
+  new->n = number;
+  old = NULL;
+  current = *head;
+
+  for (; current != NULL && current->n < number;)
+    {
+      old = current;
+      current = current->next;
+    }
+
+  new->next = current;
+
+  if (old != NULL)
+    old->next = new;
+  else
+    *head = new;
+
+  return (new);
 }
