cfb0df96c9d4aa036bc31f98ff8ee1c43112c937
Anteneh Bizuneh <antukassaw1@gmail.com>
2022-02-28 01:28:40 -0800

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+const argLen = process.argv.length;
+
+if (argLen > 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
