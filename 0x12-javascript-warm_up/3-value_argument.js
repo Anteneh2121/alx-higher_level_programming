e2e3059a53550c7a0d6fd2b0993ece967b7cfd29
Anteneh Bizuneh <antukassaw1@gmail.com>
2022-02-28 01:43:38 -0800

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+const argLen = process.argv.length;
+
+if (argLen > 2) {
+  let i;
+
+  for (i = 2; i < argLen; i++) {
+    console.log(process.argv[i]);
+  }
+} else {
+  console.log('No argument');
+}
