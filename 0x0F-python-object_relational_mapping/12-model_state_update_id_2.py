e3d53ae05aec508db0b33fb6dd73535cd04099b2
Anteneh Bizuneh <antukassaw1@gmail.com>
2022-03-06 06:48:03 -0800

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-script that changes the name of a State object from the database hbtn_0e_6_usa
-    - script should take 3 arguments: mysql username,
-    mysql password and database name
-    - uses the module SQLAlchemy
-    - import State and Base from model_state
-    - script connecta to a MySQL server running on localhost at port 3306
-    - Change the name of the State where id = 2 to New Mexico
+changes the name of the State object where id=2 to New Mexico from a database
 """
 
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
 
-if __name__ == '__main__':
-    import sys
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Session = sessionmaker(bind=engine)
-    s = Session()
-
-    state = s.merge(State(id=2))
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    state = session.query(State).filter_by(id=2).first()
     state.name = "New Mexico"
-    s.commit()
+    session.commit()
+    session.close()
